bdecacf8af22c14b4bd509a486b38e32a1d55335
Saveload <saveload65@gmail.com>
2019-09-20 10:11:57 +0300

    made bin brain-progression.js

---
+++ src/bin/games/brain-progression.js
@@ -0,0 +1,8 @@
+import {
+  game, randomProgression, randomProgressionToString, progressionVerify,
+} from '../..';
+
+console.log('Welcome to the Brain Games!');
+console.log('What number is missing in the progression?\n');
+
+game(randomProgression, progressionVerify, randomProgressionToString);
