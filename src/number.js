f8c23a7af27c41e45ba7e13bf5a423f7a437276d
Saveload <saveload65@gmail.com>
2019-10-14 12:06:18 +0300

    deleted unnecessary things

--- src/number.js
+++
@@ -1,8 +0,0 @@
-
-const genRandomNumber = () => {
-  const upperLimitForRandomNum = 50;
-  const bottomLimitForRandomNum = 1;
-  return Math.floor(Math.random() * upperLimitForRandomNum) + bottomLimitForRandomNum;
-};
-
-export default genRandomNumber;
